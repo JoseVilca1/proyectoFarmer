69009af64edeb63c544e8f108b21b9cfa5f1d38f
Dreyuix <pordrey@gmail.com>
2024-11-20 18:30:16 -0500

    version1.0

---
+++ src/App.js
@@ -0,0 +1,33 @@
+//import logo from './imagenes/foto1.jfif';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span>Informe de Datos</span>
+        <div className="superior">
+        <h1>Proyecto</h1>
+        </div>
+        <div className="inferior">
+          <h2>Contenido</h2>
+        </div>
+
+        <footer>
+          <h4>Desarrollado : Ing. Melendez</h4>
+          <h5>Celular:959818192</h5>
+        </footer>
+
+
+                
+        
+      </header>
+
+     
+
+    </div>
+  );
+}
+
+export default App;
